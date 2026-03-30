b1e7314ff2c29d2250a4dbaffbefec5cd8101480
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-03-31 00:04:09 +0200

    updated the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,53 @@
 const Box = (props)=>{
-  console.log(props);
-  return <div>
-    i am a box
+  const {label , bgcolor , textcolor , large} = props;
+  return <div style={{backgroundColor:props.bgcolor, color:props.textcolor,
+    fontSize: props.large ? "30px" : "16px"
+   }}>
+    {label}
   </div>
 }
 
 
-
-
 const App = ()=>{
   return <div>
-    <Box  label="my name is box" bgcolor="blue" />
+    <Box  label="my name is box" 
+     bgcolor="blue" 
+     textcolor="white"
+     large={true} 
+    />
     todo
   </div>;
 
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
