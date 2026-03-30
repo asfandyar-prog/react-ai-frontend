6fe5934f94b8d9ca025d50b3165e8a1a35fb789f
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-03-31 01:16:24 +0200

    created the MyComponent File

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
-
-const App = () => {
+const App=()=>{
   return <div>
-    <div className="text-rose-300 bg-red-700">
-      One
+    <div className="border-2 border-red-500 p-4 m-4">
+      Hello
     </div>
-    <div className="text-slate-400 border-4 border-black">
-      two
+    <div className="border-2 border-green-500 px-2  pb-4 m-4">
+      World
     </div>
-    <div className="text-2xl bg-emerald-200 text-blue-600">
-      Three
-    </div>
-    <div className="text-xl bg-purple-500 border-4 border-sky-500">
-      Four
-    </div>
-    <div className="text-xl bg-purple-500">
-      Five
-    </div>
-  </div>;
-};
+  </div>
+}
 
 export default App; 
 
