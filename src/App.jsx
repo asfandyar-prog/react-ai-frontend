fe9d2e386f91e4d6c8ec585828d4efa60ee9cfb7
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-03-31 01:08:58 +0200

    created the MyComponent File

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+
 const App = () => {
   return <div>
-    <div className="text-rose-300 bg-red-300">
-      Hello, Tailwind CSS!
+    <div className="text-rose-300 bg-red-700">
+      One
     </div>
-  </div>
-}
+    <div className="text-slate-400 border-4 border-black">
+      two
+    </div>
+    <div className="text-2xl bg-emerald-200 text-blue-600">
+      Three
+    </div>
+    <div className="text-xl bg-purple-500 border-4 border-sky-500">
+      Four
+    </div>
+    <div className="text-xl bg-purple-500">
+      Five
+    </div>
+  </div>;
+};
 
 export default App; 
 
