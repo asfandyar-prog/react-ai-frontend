93396d29b83058ccbda1973818036e298056dc13
Asfand Yar <asfandyar@mailbox.unideb.hu>
2026-03-31 10:59:09 +0200

    updated the App File

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-const App=()=>{
-  return <div>
-    <div className="border-2 border-red-500 p-4 m-4">
-      Hello
-    </div>
-    <div className="border-2 border-green-500 px-2  pb-4 m-4">
-      World
-    </div>
-  </div>
-}
-
-export default App; 
+const App = ()=>{
+  const boolean = false;
+  return <div className = {`${boolean? "bg-green-600" : "bg-red-500"}`}>
+    todo
+  </div>;
+};
 
 
-
+export default App;
 
 
 
